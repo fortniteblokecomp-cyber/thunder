5914eb1a5f2abeeefd908362aada126ef19ce82d
bloke <fortniteblokecomp@gmail.com>
2026-07-24 04:03:52 -0400

    changes

---
+++ Games/scriptBase.lua
@@ -0,0 +1,58 @@
+local HttpService = game:GetService("HttpService")
+
+export type Config = {
+	title: string?;
+	tabs: {any};
+	toggles: {[string]: {any}};
+	sliders: {[string]: {any}};
+	
+	[string]: any;
+}
+
+const scriptBase = {}
+
+-- Private
+const function decodeConfig(body: string) : Config
+	local _decoded = HttpService:JSONDecode(body);
+	assert(type(_decoded) == "table", "game config must be a json Object");
+	
+	return _decoded:: string;
+end
+
+-- Public
+
+function scriptBase:addTask(base, task: RBXScriptConnection)
+	assert(task.Connected, "cannot register and disconnected task");
+	
+	table.insert(base.tasks, task);
+	
+	return task;
+end
+
+function scriptBase:changeTab(base, newTab)
+	
+end
+
+function scriptBase:toggle(base, _instance)
+	
+end
+
+function scriptBase:loadConfig(configBaseUrl: string)
+	local ok, bodyOrError = pcall(function()
+		return HttpService:GetAsync(configBaseUrl, true);
+	end)
+	assert(ok, "unable to fetch game config");
+	
+	local decodeOk, configOrError = pcall(decodeConfig, bodyOrError);
+	assert(decodeOk, "invalid to fetch game config");
+	
+	base.config = configOrError;
+	base.tabs = configOrError.tabs or {};
+	base.toggles = configOrError.toggles or {};
+	base.sliders = configOrError.sliders or {};
+	
+	return configOrError
+end
+
+
+return scriptBase
---
+++ Games/thunder.lua
@@ -0,0 +1,75 @@
+-- Services
+
+local HttpService = game:GetService("HttpService")
+
+
+
+-----------------------------------------------------
+
+const scriptBase = loadstring()
+
+-------------------------------------------------------
+
+
+const thunder = setmetatable({}, {__index = scriptBase})
+
+
+local bin = {
+	lunaGit = nil;
+}
+
+
+
+function thunder.new()
+	local self = setmetatable({}, {__index = thunder});
+	
+	self.dataBase = {};
+	
+	self.toggles = {};
+	self.sliders = {};
+	
+	self.tasks = {} :: {RBXScriptConnection}
+	
+	self.tabs = {}
+	self.config = {}
+	
+	return self 
+end
+
+
+
+
+function thunder:mount()
+	bin.lunaGit = loadstring(game:HttpGet("https://raw.nebulasoftworks.xyz/luna", true))()
+end
+
+function thunder:render()
+	local git = bin.lunaGit
+	
+	warn(git)
+	self:addTask(self, task.spawn(function()
+		bin["interface"] = git:CreateWindow({
+			Name = "thunder"
+		})
+	end))
+
+end;
+
+
+function scriptBase:loadGameConfig(configBaseUrl: string, placeId: number)
+	const configUrl = `{configBaseUrl}/{placeId}.json`;
+
+	return self:loadConfig(configUrl)
+end
+
+function thunder:cleanup()
+	for _, task in self.tasks do
+		task:Disconnect()
+	end
+	
+	table.clear(self.tasks)
+	table.clear(self.bin)
+	return setmetatable(self, nil)
+end
+
+return thunder;
